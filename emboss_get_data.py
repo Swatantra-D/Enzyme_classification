0fc8fe78b0d50c551a7cfc344793629cc57ba522
Swatantra-D <dharaswat@gmail.com>
2023-11-17 15:58:09 +0530

    bug fixes

--- emboss_get_data.py
+++ emboss_get_data.py
@@ -1,8 +1,15 @@
-import os
-dir_list = os.listdir()
-for dir in dir_list:
+## This script is used to automatically get the data from emboss pepstat web server
+# You'll require the emboss_pepstats.py on path to successfully get the data
+# This script will loop over all your sequence files containint 500 sequences(limit for the emboss pepstats server) and get the peptide statistics results
+
+import os # import os module for directory listing and running commands
+dir_list = os.listdir() # list all the 500 seq files in the current directory
+t_dir = [] # keep a list of files which had a problem running
+for dir in dir_list: # loop over all the files
     try:
-        command = r'python ./emboss_pepstats.py --email swatantra.5.dhara@gmail.com --sequence ' + dir
+        command = r'python ./emboss_pepstats.py --email youremail@domain.com --sequence' + dir # 
         os.system(command)
     except:
-        pass
+        t_dir.append(dir)
+        continue
+print(t_dir) # print the list of files which faced trouble
